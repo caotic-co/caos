494965cf6ba7967679ceb93138200bf41a8230d3
Camilo Ospina <camilo.ospinaa@gmail.com>
2019-04-26 22:21:38 -0500

    Initial Commit
    First commit that establishes the structure of source code, so that it can be uploaded to the Python Package Index (PyPI) or installed manually using Python.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# caos
+<p align="center">
+    <a href="https://github.com/ospinakamilo/caos" target="_blank">
+        <img src="https://github.com/ospinakamilo/caos/blob/master/docs/img/caos_logo.svg" height="100px">
+    </a>
+    <h1 align="center">CAOS</h1>
+    <br>
+</p>
 Simple Dependencies Manager for Python3 Projects
+
+REQUIREMENTS
+------------
+
+The minimum requirements for this project is to have installed Python >= 3.4.
+
+
+Installation using PIP
+------------
+Run the following command if you have access to pip
+~~~
+pip install caos
+~~~
+or run this command to use pip as a python module
+
+~~~
+python -m pip install caos
+~~~
+
+
+Manual Installation
+------------
+Clone the repository using the following command:
+~~~
+git clone https://github.com/ospinakamilo/caos
+~~~
+
+In the location where you cloned the repository run the next command:
+~~~
+python setup.py bdist_wheel
+~~~
+This will create a folder called 'dist' which contatins the module to install.
+To install the module in your local system use the following commmand (be sure to validate the version number in the file):
+~~~
+python -m pip install dist/caos-x.x-py3-none-any.whl
+~~~
---
+++ setup.py
@@ -0,0 +1,41 @@
+import sys
+from setuptools import find_packages, setup
+
+full_description = '''
+    Please take a look at our documentation for how to install and use caos:
+        
+        * `GitHub page`_
+        .. _GitHub page: https://github.com/ospinakamilo/caos/
+'''
+
+
+setup(
+    name='caos',  
+    version='1.0',
+    author="Team Camilo",
+    author_email="camilo.ospinaa@gmail.com",
+    description="caos - Simple Dependencies Manager for Python3 Projects",
+    long_description=full_description,
+    url="https://github.com/ospinakamilo/caos/",
+    keywords='distutils easy_install egg setuptools wheel virtualenv dependencies manager ppm',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+
+    package_dir={"": "src"},
+
+    packages=find_packages(
+        where="src",
+        exclude=["docs", "tests"],
+    ),    
+
+    entry_points={
+        "console_scripts": ["caos=caos:console"],
+    },
+
+    python_requires='>3, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    
+ )
