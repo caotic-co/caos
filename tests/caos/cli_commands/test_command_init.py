c1634d1014af88cac71576aac08ee8806df9ef19
Camilo Ospina <camilo.ospinaa@gmail.com>
2020-03-29 17:11:43 -0500

    Use get_current_dir() function the init command unittests

--- tests/caos/cli_commands/test_command_init.py
+++ tests/caos/cli_commands/test_command_init.py
@@ -6,6 +6,7 @@ from io import StringIO
 from caos.cli_commands import command_init
 from caos.style.console.tools import escape_ansi
 from caos.utils.os import is_posix_os, is_win_os
+from caos.utils.working_directory import get_current_dir
 
 
 _CREATING_VIRTUAL_ENVIRONMENT_MESSAGE = "[caos] init --> Creating virtual environment..."
@@ -16,10 +17,11 @@ _YAML_CREATED_MESSAGE = "[caos] init --> SUCCESS: 'caos.yml' created"
 _YAML_EXISTS_MESSAGE = "[caos] init --> INFO: 'caos.yml' already exists"
 _UPDATE_YAML_REMINDER_MESSAGE = "[caos] init --> INFO: Don't forget to update the 'caos.yml' file to point to the right virtual environment"
 
-_PYTHON_PATH_VENV_POSIX = "tmp/venv/bin/python"
-_PYTHON_PATH_VENV_WIN = "tmp/venv/Scripts/python.exe"
-_PIP_PATH_VENV_POSIX = "tmp/venv/bin/pip"
-_PIP_PATH_VENV_WIN = "tmp/venv/Scripts/pip.exe"
+_CURRENT_DIR = get_current_dir()
+_PYTHON_PATH_VENV_POSIX = os.path.abspath(_CURRENT_DIR+"/venv/bin/python")
+_PYTHON_PATH_VENV_WIN = os.path.abspath(_CURRENT_DIR+"/venv/Scripts/python.exe")
+_PIP_PATH_VENV_POSIX = os.path.abspath(_CURRENT_DIR+"/venv/bin/pip")
+_PIP_PATH_VENV_WIN = os.path.abspath(_CURRENT_DIR+"/venv/Scripts/pip.exe")
 
 class TestCommandInit(unittest.TestCase):
 
@@ -36,8 +38,8 @@ class TestCommandInit(unittest.TestCase):
     def test_init_no_args(self):
         command_init.entry_point(args=[])
         messages: str = escape_ansi(self.new_stdout.getvalue())
-        self.assertEqual(os.path.isdir("tmp/venv"), True)
-        self.assertEqual(os.path.isfile("tmp/caos.yml"), True)
+        self.assertTrue(os.path.isdir(os.path.abspath(_CURRENT_DIR+"/venv")))
+        self.assertTrue(os.path.isfile(os.path.abspath(_CURRENT_DIR+"/caos.yml")))
 
         self.assertIn(_CREATING_VIRTUAL_ENVIRONMENT_MESSAGE, messages)
         messages: str = messages.replace(_CREATING_VIRTUAL_ENVIRONMENT_MESSAGE, "", 1)
@@ -53,8 +55,8 @@ class TestCommandInit(unittest.TestCase):
     def test_init_my_env(self):
         command_init.entry_point(args=["my_env"])
         messages: str = escape_ansi(self.new_stdout.getvalue())
-        self.assertEqual(os.path.isdir("tmp/my_env"), True)
-        self.assertEqual(os.path.isfile("tmp/caos.yml"), True)
+        self.assertTrue(os.path.isdir(os.path.abspath(_CURRENT_DIR+"/my_env")))
+        self.assertTrue(os.path.isfile(os.path.abspath(_CURRENT_DIR+"/caos.yml")))
 
         self.assertIn(_CREATING_VIRTUAL_ENVIRONMENT_MESSAGE, messages)
         messages: str = messages.replace(_CREATING_VIRTUAL_ENVIRONMENT_MESSAGE, "", 1)
@@ -71,8 +73,8 @@ class TestCommandInit(unittest.TestCase):
         command_init.entry_point(args=[])
         command_init.entry_point(args=[])
         messages: str = escape_ansi(self.new_stdout.getvalue())
-        self.assertEqual(os.path.isdir("tmp/venv"), True)
-        self.assertEqual(os.path.isfile("tmp/caos.yml"), True)
+        self.assertTrue(os.path.isdir(os.path.abspath(_CURRENT_DIR+"/venv")))
+        self.assertTrue(os.path.isfile(os.path.abspath(_CURRENT_DIR+"/caos.yml")))
         
         self.assertIn(_CREATING_VIRTUAL_ENVIRONMENT_MESSAGE, messages)
         messages: str = messages.replace(_CREATING_VIRTUAL_ENVIRONMENT_MESSAGE, "", 1)
@@ -95,8 +97,8 @@ class TestCommandInit(unittest.TestCase):
         command_init.entry_point(args=[])
         command_init.entry_point(args=["my_env"])
         messages: str = escape_ansi(self.new_stdout.getvalue())
-        self.assertEqual(os.path.isdir("tmp/venv"), True)
-        self.assertEqual(os.path.isfile("tmp/caos.yml"), True)
+        self.assertTrue(os.path.isdir(os.path.abspath(_CURRENT_DIR+"/venv")))
+        self.assertTrue(os.path.isfile(os.path.abspath(_CURRENT_DIR+"/caos.yml")))
         
         self.assertIn(_CREATING_VIRTUAL_ENVIRONMENT_MESSAGE, messages)
         messages: str = messages.replace(_CREATING_VIRTUAL_ENVIRONMENT_MESSAGE, "", 1)
