cc538a97245886a2921c36000f6d3f05fc471038
Camilo Ospina <camilo.ospinaa@gmail.com>
2020-04-08 21:38:55 -0500

    Add ANSICON to allow terminal colors in windows

--- caos/_internal/console/tools.py
+++ caos/_internal/console/tools.py
@@ -1,6 +1,8 @@
 import os
 import re
 import sys
+from caos._internal.utils.os import is_posix_os, is_win_os
+from caos._third_party.ansi189 import ansicon
 
 
 def supports_color() -> bool:
@@ -8,8 +10,8 @@ def supports_color() -> bool:
     Returns True if the running system's terminal supports color, and False
     otherwise.
     """
-    supported_platform = sys.platform != 'Pocket PC' and (sys.platform != 'win32' or 'ANSICON' in os.environ)
-    is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty() # isatty is not always implemented, #6223.
+    supported_platform = is_posix_os() or os.environ.get('ANSICON') or (is_win_os() and ansicon.load())
+    is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty() # isatty is not always present
     return supported_platform and is_a_tty
 
 
